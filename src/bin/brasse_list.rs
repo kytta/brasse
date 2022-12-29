4684a31b8a2b8056d32690b1a5ef0ba2ca1c7820
Nikita Karamov <me@kytta.dev>
2022-12-29 18:23:46 +0100

    Make a CLI with clap

---
+++ src/bin/brasse_list.rs
@@ -0,0 +1,91 @@
+use clap::Command;
+use std::env;
+use std::fs;
+use std::io;
+use std::path::PathBuf;
+
+pub fn cli() -> Command {
+    Command::new("list")
+        .bin_name("brasse-list")
+        .about("List installed formulae and casks")
+}
+
+fn get_formulae() -> Result<Vec<String>, io::Error> {
+    let cellar = PathBuf::from(
+        env::var("HOMEBREW_CELLAR").unwrap_or_else(|_| "/opt/homebrew/Cellar".to_string()),
+    );
+
+    let mut formulae: Vec<String> = Vec::new();
+
+    for entry in fs::read_dir(cellar)? {
+        let entry = entry?;
+        let path = entry.path();
+        let file_name = path.file_name();
+
+        let formula_os_str = match file_name {
+            Some(f) => f,
+            None => continue,
+        };
+
+        let formula_name = match formula_os_str.to_str() {
+            Some(name) => name,
+            None => continue,
+        };
+
+        if formula_name.starts_with('.') {
+            continue;
+        }
+
+        formulae.push(formula_name.to_string())
+    }
+    formulae.sort();
+
+    Ok(formulae)
+}
+
+fn get_casks() -> Result<Vec<String>, io::Error> {
+    let caskroom = PathBuf::from("/opt/homebrew/Caskroom".to_string());
+
+    let mut casks: Vec<String> = Vec::new();
+
+    for entry in fs::read_dir(caskroom)? {
+        let entry = entry?;
+        let path = entry.path();
+        let file_name = path.file_name();
+
+        let cask_os_str = match file_name {
+            Some(f) => f,
+            None => continue,
+        };
+
+        let cask_name = match cask_os_str.to_str() {
+            Some(name) => name,
+            None => continue,
+        };
+
+        if cask_name.starts_with('.') {
+            continue;
+        }
+
+        casks.push(cask_name.to_string())
+    }
+    casks.sort();
+
+    Ok(casks)
+}
+
+pub fn main() {
+    let _matches = cli().get_matches();
+
+    eprintln!("==> Formulae");
+
+    for formula in get_formulae().unwrap() {
+        eprintln!("{}", formula);
+    }
+
+    eprintln!("==> Casks");
+
+    for cask in get_casks().unwrap() {
+        eprintln!("{}", cask);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use bin::brasse_list;
+use clap::Command;
+
+mod bin {
+    pub mod brasse_list;
+}
+
+fn cli() -> Command {
+    Command::new("brasse")
+        .about("Homebrew but better.")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(brasse_list::cli())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = cli().get_matches();
+
+    match matches.subcommand() {
+        Some(("list", _)) => bin::brasse_list::main(),
+        _ => unreachable!(),
+    };
 }
